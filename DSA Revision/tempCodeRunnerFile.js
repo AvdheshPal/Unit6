2ab5b0b5629626bad38c00ae697ecf13d30d5cd4
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-02-02 00:27:21 +0530

    Delete tempCodeRunnerFile.js

--- DSA Revision/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-for(let i = 1; i < arr.length-1; i++) {
-//     // right -= Math.abs(arr[i]);
-//     // left+=arr[i-1];
-//     // if(left == right){
-//     //     ans = i;
-//     //     break;
-//     // }
-//     // // console.log(left,right);
-// }
