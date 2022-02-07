332236eddcd1055134510d60ea0a4c98500bc137
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-02-07 12:25:35 +0530

    Update HighFrequency.js

--- DSA Revision/HighFrequency.js
+++ DSA Revision/HighFrequency.js
@@ -31,3 +31,39 @@ function ok(n, arr) {
         console.log(min);
     }
 }
+
+
+function runProgram(input) {
+    input = input.trim().split('\n')
+    let tc = input[0];
+    let line = 1;
+    for (let i = 0; i < tc; i++) {
+        let n = +input[line++].trim();
+        let arr = input[line++].trim().split(' ').map(Number);
+        ok(n, arr)
+    }
+}
+if (process.env.USERNAME === "awdhesh") {
+    runProgram(`2
+    6
+    1 4 4 4 5 3
+    11
+    1 2 3 4 5 4 3 2 1 3 4`);
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
