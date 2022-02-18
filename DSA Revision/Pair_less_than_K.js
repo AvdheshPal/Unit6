5277168fa67f9c7c0d75825ee24ddcef7e488574
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-02-18 10:23:56 +0530

    Update Pair_less_than_K.js

--- DSA Revision/Pair_less_than_K.js
+++ DSA Revision/Pair_less_than_K.js
@@ -1,5 +1,38 @@
+function getThePair(n,arr,k){
+  let i = 0;
+  let j = 1;
+  let ans = -1;
+  while(i < n && j < n){
+      let s = arr[i] + arr[j];
+       
+      if(s < k){
+          if(s > ans){
+              ans = s;
+          }
+          j++
+      }
+      else if(s > k){
+          i++;
+      }
+      else{
+          i++;
+          j++;
+      }
+
+  }
+  console.log(ans);
+}
+
 function runProgram(input) {
-  
+  input = input.trim().split('\n');
+  let tc = +input[0].trim();
+  let line = 1;
+  for(let i = 0; i < tc; i++) {
+      let n = +input[line++].trim()
+      let arr = input[line++].trim().split(' ').map(Number);
+      let k = +input[line++].trim()
+      getThePair(n,arr,k)
+  }
   
  }
  if (process.env.USERNAME === "awdhesh") {
