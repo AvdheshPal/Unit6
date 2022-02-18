597dc4f64888dbf94f927be35327f33148bbd022
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-02-18 10:47:49 +0530

    Update Pair_less_than_K.js

--- DSA Revision/Pair_less_than_K.js
+++ DSA Revision/Pair_less_than_K.js
@@ -1,63 +1,62 @@
-function getThePair(n,arr,k){
-  let i = 0;
-  let j = 1;
-  let ans = -1;
-  while(i < n && j < n){
-      let s = arr[i] + arr[j];
-       
-      if(s < k){
-          if(s > ans){
-              ans = s;
-          }
-          j++
-      }
-      else if(s > k){
-          i++;
-      }
-      else{
-          i++;
-          j++;
-      }
+function getThePair(n, arr, k) {
+    let i = 0;
+    let j = 1;
+    let ans = -1;
+    while (i < n && j < n) {
+        let s = arr[i] + arr[j];
+        if (s < k) {
+            if (s > ans) {
+                ans = s;
+            }
+            j++
+        }
+        else if (s > k) {
+            i++;
+        }
+        else {
+            i++;
+            j++;
+        }
 
-  }
-  console.log(ans);
+    }
+    console.log(ans);
 }
 
 function runProgram(input) {
-  input = input.trim().split('\n');
-  let tc = +input[0].trim();
-  let line = 1;
-  for(let i = 0; i < tc; i++) {
-      let n = +input[line++].trim()
-      let arr = input[line++].trim().split(' ').map(Number);
-      let k = +input[line++].trim()
-      getThePair(n,arr,k)
-  }
-  
- }
- if (process.env.USERNAME === "awdhesh") {
-   runProgram(`2
+    input = input.trim().split('\n');
+    let tc = +input[0].trim();
+    let line = 1;
+    for (let i = 0; i < tc; i++) {
+        let n = +input[line++].trim()
+        let arr = input[line++].trim().split(' ').map(Number);
+        let k = +input[line++].trim()
+        getThePair(n, arr, k)
+    }
+
+}
+if (process.env.USERNAME === "awdhesh") {
+    runProgram(`2
    5
    1 2 3 4 5
    7
    3
    30 10 20
    15`);
- } else {
-   process.stdin.resume();
-   process.stdin.setEncoding("ascii");
-   let read = "";
-   process.stdin.on("data", function (input) {
-     read += input;
-   });
-   process.stdin.on("end", function () {
-     read = read.replace(/\n$/, "");
-     read = read.replace(/\n$/, "");
-     runProgram(read);
-   });
-   process.on("SIGINT", function () {
-     read = read.replace(/\n$/, "");
-     runProgram(read);
-     process.exit(0);
-   });
- }
+} else {
+    process.stdin.resume();
+    process.stdin.setEncoding("ascii");
+    let read = "";
+    process.stdin.on("data", function (input) {
+        read += input;
+    });
+    process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+    });
+    process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+        runProgram(read);
+        process.exit(0);
+    });
+}
