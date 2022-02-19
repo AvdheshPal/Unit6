75adc26257fef77ef74fa08a17aa447c2a9f5461
AvdheshPal <87435192+AvdheshPal@users.noreply.github.com>
2022-02-19 20:07:10 +0530

    School Teacher Manager

---
+++ Evaluation/Full Stack Assignment/backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const mongoose = require('mongoose')
+
+
+const app = express()
+
+app.use(express.json())
+
+const connect = require('./src/config/db')
+
+app.listen(2345 , async (req,res)=>{
+    await connect();
+    console.log("listening port 2345")
+})
---
+++ Evaluation/Full Stack Assignment/frontend/school/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Login} from './components/Login/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Masai School</h1>
+     <Login/>
+    </div>
+  );
+}
+
+export default App;
